9e46b6bcb21c42c4b0b47f483521d9adf62e93cd
theozebua <ronizebua.rz@gmail.com>
2022-11-24 19:30:20 +0700

    Return a view from a controller

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,21 +4,14 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use App\Models\User;
+use Theozebua\OzeFramework\View\View;
+use function view;
+use function view_path;
 
 final class HomeController extends Controller
 {
-    final public function index(User $user): void
+    final public function index(): View
     {
-        echo 'From: ' . __METHOD__ . PHP_EOL;
-
-        foreach ($user->all() as $u) {
-            echo "
-                <ul>
-                    <li>{$u->name}</li>
-                    <li>{$u->email}</li>
-                </ul>
-            ";
-        }
+        return view(view_path('welcome'));
     }
 }
