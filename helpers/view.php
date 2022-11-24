8dbe84dcbb8e7aa65f14586dfb1dbcc3f43f85c8
theozebua <ronizebua.rz@gmail.com>
2022-11-24 19:29:16 +0700

    Create config and helper files

---
+++ helpers/view.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Theozebua\OzeFramework\View\View;
+
+if (!function_exists('view')) {
+    /**
+     * Render a view.
+     * 
+     * @param string $view
+     * @param array $data
+     * 
+     * @return View
+     */
+    function view(string $view, array $data = []): View
+    {
+        return View::make($view, $data);
+    }
+}
