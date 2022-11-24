6b4a2d784e52be33762619b93b46485e7821f9b4
theozebua <ronizebua.rz@gmail.com>
2022-11-24 19:29:02 +0700

    Create welcome view

---
+++ views/welcome.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Oze Framework</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <style>
+        html {
+            height: 100%;
+            font-family: 'Poppins', sans-serif;
+        }
+
+        body {
+            height: 100%;
+            display: grid;
+            place-items: center;
+            background: #e5e5e5;
+            color: #121212;
+        }
+
+        .wrapper {
+            min-height: 2.5rem;
+            background: #ffffff;
+            padding: 2.5rem;
+            border-radius: .5rem;
+            box-shadow: 2px 2px rgba(0, 0, 0, .1);
+        }
+    </style>
+</head>
+
+<body>
+    <div class="wrapper">
+        <h1>Welcome to Oze Framework</h1>
+    </div>
+</body>
+
+</html>
