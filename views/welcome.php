2fc362bd27a459f291d73843e0b20c696aaa2113
theozebua <ronizebua.rz@gmail.com>
2022-11-25 18:07:50 +0700

    Modify welcome view

--- views/welcome.php
+++ views/welcome.php
@@ -12,6 +12,12 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
         html {
             height: 100%;
             font-family: 'Poppins', sans-serif;
@@ -30,7 +36,8 @@
             background: #ffffff;
             padding: 2.5rem;
             border-radius: .5rem;
-            box-shadow: 2px 2px rgba(0, 0, 0, .1);
+            box-shadow: 0 2px rgba(0, 0, 0, .1);
+            text-align: center;
         }
     </style>
 </head>
@@ -38,6 +45,7 @@
 <body>
     <div class="wrapper">
         <h1>Welcome to Oze Framework</h1>
+        <p>Just a simple mvc framework for beginners.</p>
     </div>
 </body>
 
