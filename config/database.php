ca5b5aca63b83e2ef1a53925c962faaf62d6eaa0
theozebua <ronizebua.rz@gmail.com>
2022-12-05 16:20:53 +0700

    Create database configuration

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'dsn_prefix' => 'mysql',
+    'host'       => 'localhost',
+    'port'       => '3306',
+    'charset'    => 'utf8mb4',
+    'database'   => '',
+    'username'   => 'root',
+    'password'   => '',
+];
